ce7b8d133e3fc387f189e6cc0f96ce5c74c42d4f
Howard Chang <howard3433@gmail.com>
2015-11-10 11:23:34 +0800

    Add solution to question 107

---
+++ Easy/107_Binary_Tree_Level_Order_Traversal_II.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    if (!root) {
+        return [];
+    }
+    if (!root.left && !root.right) {
+        return [[root.val]];
+    }
+
+    var result = [];
+    var leftTrees = levelOrderBottom(root.left).reverse();
+    var rightTrees = levelOrderBottom(root.right).reverse();
+    var longer = leftTrees.length > rightTrees.length ? leftTrees : rightTrees;
+
+    longer.forEach(function(v, i) {
+        if (leftTrees[i] && rightTrees[i]) {
+            result.push(leftTrees[i].concat(rightTrees[i]));
+        } else if (leftTrees[i] && !rightTrees[i]){
+            result.push(leftTrees[i]);
+        } else {
+            result.push(rightTrees[i]);
+        }
+    });
+
+    return result.reverse().concat([[root.val]]);
+};
