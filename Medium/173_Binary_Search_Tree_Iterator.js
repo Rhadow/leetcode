ecfbb7cf02dee774add556707e9d197d71a2bd29
Rhadow <howard3433@gmail.com>
2015-11-19 10:02:45 +0800

    Add solution to question 173

---
+++ Medium/173_Binary_Search_Tree_Iterator.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for binary tree
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @constructor
+ * @param {TreeNode} root - root of the binary search tree
+ */
+var BSTIterator = function(root) {
+    this.stack = [];
+    var pointer = root;
+
+    while (pointer) {
+        this.stack.push(pointer);
+        pointer = pointer.left;
+    }
+};
+
+
+/**
+ * @this BSTIterator
+ * @returns {boolean} - whether we have a next smallest number
+ */
+BSTIterator.prototype.hasNext = function() {
+    return this.stack.length !== 0;
+};
+
+/**
+ * @this BSTIterator
+ * @returns {number} - the next smallest number
+ */
+BSTIterator.prototype.next = function() {
+    var node = this.stack.pop();
+    var pointer;
+
+    if (node.right) {
+        pointer = node.right;
+        while (pointer) {
+            this.stack.push(pointer);
+            pointer = pointer.left;
+        }
+    }
+    return node.val;
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * var i = new BSTIterator(root), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
