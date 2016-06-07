bbbe1010b7aaee6dcec9f2abb3f39a5f7ddee903
Howard Chang <howard3433@gmail.com>
2016-06-07 12:55:48 +0800

    Add solution to lintcode question 85

---
+++ lintcode/Easy/085_Insert_Node_in_a_Binary_Search_Tree.py
@@ -0,0 +1,46 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+class Solution:
+    """
+    @param root: The root of the binary search tree.
+    @param node: insert this node into the binary search tree.
+    @return: The root of the new binary search tree.
+    """
+    def insertNode(self, root, node):
+        # write your code here
+
+        # Iterative
+        if (root is None):
+            return node
+        parent = None
+        target = root
+        dir = None
+        while (target):
+            parent = target
+            if (target.val < node.val):
+                target = target.right
+                dir = 'right'
+            else:
+                target = target.left
+                dir = 'left'
+            if (target is None):
+                if (dir == 'left'):
+                    parent.left = node
+                else:
+                    parent.right = node
+                break
+        return root;
+
+        # Recursive
+        # if (root is None):
+        #     return node
+        # if (root.val > node.val):
+        #     root.left = self.insertNode(root.left, node)
+        # else:
+        #     root.right = self.insertNode(root.right, node)
+        # return root
