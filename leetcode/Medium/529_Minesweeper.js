93c2874905199bfb4b65cdf3599bbe08596835c7
Howard_HonestBee <howard3433@gmail.com>
2017-02-26 22:58:59 +0800

    Refactor solution of leetcode question 529

--- leetcode/Medium/529_Minesweeper.js
+++ leetcode/Medium/529_Minesweeper.js
@@ -4,78 +4,41 @@
  * @return {character[][]}
  */
 var updateBoard = function(board, click) {
-    // Original Solution
     const clickedRow = click[0];
     const clickedCol = click[1];
+
     if (board[clickedRow][clickedCol] === 'M') {
         board[clickedRow][clickedCol] = 'X';
     } else if (board[clickedRow][clickedCol] === 'E') {
         const surroundingBombs = countSurroundingBombs(board, clickedRow, clickedCol);
         if (surroundingBombs === 0) {
+            let rows = [clickedRow - 1, clickedRow, clickedRow + 1].filter(row => row >= 0 && row < board.length);
+            let cols = [clickedCol - 1, clickedCol, clickedCol + 1].filter(col => col >= 0 && col < board[clickedRow].length);
             board[clickedRow][clickedCol] = 'B';
-            // Top
-            if (clickedRow - 1 >= 0) {
-                if (clickedCol - 1 >= 0) {
-                    updateBoard(board, [clickedRow - 1, clickedCol - 1]);
+            for (let i = 0; i < rows.length; i++) {
+                for (let j = 0; j < cols.length; j++) {
+                   if (rows[i] !== clickedRow || cols[j] !== clickedCol) {
+                        updateBoard(board, [rows[i], cols[j]]);
+                    }
                 }
-                if (clickedCol + 1 < board[clickedRow].length) {
-                    updateBoard(board, [clickedRow - 1, clickedCol + 1]);
-                }
-                updateBoard(board, [clickedRow - 1, clickedCol]);
-            }
-            // Bottom
-            if (clickedRow + 1 < board.length) {
-                if (clickedCol - 1 >= 0) {
-                    updateBoard(board, [clickedRow + 1, clickedCol - 1]);
-                }
-                if (clickedCol + 1 < board[clickedRow].length) {
-                    updateBoard(board, [clickedRow + 1, clickedCol + 1]);
-                }
-                updateBoard(board, [clickedRow + 1, clickedCol]);
-            }
-            // Middle
-            if (clickedCol - 1 >= 0) {
-                updateBoard(board, [clickedRow, clickedCol - 1]);
-            }
-            if (clickedCol + 1 < board[clickedRow].length) {
-                updateBoard(board, [clickedRow, clickedCol + 1]);
             }
         } else {
             board[clickedRow][clickedCol] = parseInt(surroundingBombs, 10);
         }
     }
-
     return board;
 };
 
 function countSurroundingBombs(board, rowIndex, colIndex) {
     let result = 0;
-    // Top
-    if (rowIndex - 1 >= 0) {
-        if (colIndex - 1 >= 0) {
-            result += (board[rowIndex - 1][colIndex - 1] === 'M') ? 1 : 0;
-        }
-        if (colIndex + 1 < board[rowIndex].length) {
-            result += (board[rowIndex - 1][colIndex + 1] === 'M') ? 1 : 0;
-        }
-        result += (board[rowIndex - 1][colIndex] === 'M') ? 1 : 0;
-    }
-    // Bottom
-    if (rowIndex + 1 < board.length) {
-        if (colIndex - 1 >= 0) {
-            result += (board[rowIndex + 1][colIndex - 1] === 'M') ? 1 : 0;
-        }
-        if (colIndex + 1 < board[rowIndex].length) {
-            result += (board[rowIndex + 1][colIndex + 1] === 'M') ? 1 : 0;
-        }
-        result += (board[rowIndex + 1][colIndex] === 'M') ? 1 : 0;
-    }
-    // Middle
-    if (colIndex - 1 >= 0) {
-        result += (board[rowIndex][colIndex - 1] === 'M') ? 1 : 0;
-    }
-    if (colIndex + 1 < board[rowIndex].length) {
-        result += (board[rowIndex][colIndex + 1] === 'M') ? 1 : 0;
-    }
+    let rows = [rowIndex - 1, rowIndex, rowIndex + 1].filter(row => row >= 0 && row < board.length);
+    let cols = [colIndex - 1, colIndex, colIndex + 1].filter(col => col >= 0 && col < board[rowIndex].length);
+    rows.forEach(row => {
+        cols.forEach(col => {
+            if (row !== rowIndex || col !== colIndex) {
+                result += (board[row][col] === 'M') ? 1 : 0;
+            }
+        });
+    });
     return result;
 }
